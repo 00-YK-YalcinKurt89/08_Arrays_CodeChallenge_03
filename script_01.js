5bcfaa700be9b6c85dea1c1518b4126db522bee9
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-19 14:01:38 +0200

    Challenge

--- script_01.js
+++ script_01.js
@@ -12,17 +12,34 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
+
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
 function getSentence(arr,op) {
+    const GAP = " ";
+    const PUNCT = ".";
+    const question = "?";
+    const exclamation = "!"
+
+    let sentence = prompt("Bitte gebe S, Q oder E ein:")
+
+    switch (op) {
+        case "S":
+            return arr.join(GAP) + PUNCT;
+            break;
+        case "Q":
+            return arr.join(GAP) + question;
+            break;
+        case "E":
+            return arr.join(GAP) + exclamation;
+    }
    
-   
-   
-     return "toDo";
+     
 }
+
 
 
 
