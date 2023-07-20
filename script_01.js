6412a408defac930fe2fe039c683b5812e6e4df8
Yalcin <yalcin.coding@gmail.com>
2023-07-20 09:21:27 +0200

    3 Lösungen eingefügt

--- script_01.js
+++ script_01.js
@@ -26,25 +26,113 @@ function getSentence(arr,op) {
 
     let sentence = prompt("Bitte gebe S, Q oder E ein:")
 
+    for (let i = 0; i < array.length; i++) {
+        if (i != arr.length-1) {
+            str += arr
+            
+        }
+        
+    }
+}"use strict";
+/*** Mini-Challenge  */
+// Satzbau + Punktuation
+
+// Vereinbarung:
+// "S" (sentence) --> "."  Ich bin Max.
+// "Q" (question) --> "?"  Bist du Max?
+// "E" (exclamation) --> "!"  Ich bin!
+
+// Parameter --> funktion getSentence([ ...], "Q"){ ...}
+const GAP = " ";
+const PUNCTUATION = {S: '.', Q: '?', E: '!'}
+const ERROR_PUNC = "FEHLER! Satzzeichen nicht erkannt."
+
+
+/*** Funktion mit Array als Parameter */
+// mögliche Tests:
+
+
+
+
+
+
+/* ERSTE VARIANTE */ 
+
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei",
+                    "diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+// output(getSentence(["Ich","bin","Max"],"A"));
+
+// function getSentence(arr,op) {
+//     // return arr.join(GAP) + getPunctuation(op);
+//     if (op in PUNCTUATION) {
+//         return arr.join(GAP) + PUNCTUATION[op];
+//     } else {
+//         return ERROR_PUNC
+//     }
+// }
+
+
+
+
+
+
+
+/** ZWEITE VARIANTE */
+
+// function getPunctuation(op) {
+//     if (op == "S") {
+//         op = "."
+//     } else if (op == "Q") {
+//         op = "?"
+//     } else if (op == "E"){
+//         op = "!"
+//     } else {
+//         op = "";
+//     }
+//     return op;
+// }
+
+
+function getSentence(arr,op) {
+    const GAP = " ";
+    const PUNCT = getPunct(op);  // . ? !
+    let str = ""; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length-1) {
+            str += arr[i] + GAP;
+        } else {
+            str += arr[i] + PUNCT; 
+        }
+    }
+     return str;
+}
+
+function getPunct(op) {
     switch (op) {
         case "S":
-            return arr.join(GAP) + PUNCT;
-
+           return "."; 
         case "Q":
-            return arr.join(GAP) + question;
-            
+            return "?";
         case "E":
-            return arr.join(GAP) + exclamation;
+            return "!";
+        default:
+            return "#";
     }
-   
-     
 }
+
+
 
 
 
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
+
 function output(outputData) {
     console.log(outputData);
 }
